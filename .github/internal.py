2add35920dfc1c88f50e947aebdd02c48ea85faa
FroggSaron <sjackso2@macalester.edu>
2024-04-10 14:03:14 -0500

    Put basic outline of what random chance should be

--- .github/internal.py
+++ .github/internal.py
@@ -1,8 +1,34 @@
 import random
 
-optionsForSlotMachine = {"A": 1,
-                         "B": 2,
-                         "C": 3,
-                         "D": 4}
+optionsForSlotMachine = { 1: "A",
+                         4: "B",
+                         3: "C",
+                         2: "D",
+                          5: "the",
+                          6: 7,
+                          7: "Neopet"}
 
-random.choice(optionsForSlotMachine.get())
+key = 0
+value = 0
+for x in range(5):
+    value = random.random()
+if value <= .2:
+    print(optionsForSlotMachine.get(1))
+elif .2 < value <= .4:
+    print(optionsForSlotMachine.get(2))
+elif .4 < value <= .6:
+    print(optionsForSlotMachine.get(3))
+elif .6 < value <= .7:
+    print(optionsForSlotMachine.get(4))
+elif .7 <value <= .8:
+    print(optionsForSlotMachine.get(5))
+elif.8 <value <= .97:
+    print(optionsForSlotMachine.get(6))
+elif .97 < value <= 1:
+    print(optionsForSlotMachine.get(7))
+# When stop button is pressed
+#
+
+
+
+
