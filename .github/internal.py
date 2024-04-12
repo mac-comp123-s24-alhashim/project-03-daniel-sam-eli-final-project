886e4f50c2ece54fc8ce19a156c31e126c98a4a3
FroggSaron <sjackso2@macalester.edu>
2024-04-12 13:26:56 -0500

    Update internal.py

--- .github/internal.py
+++ .github/internal.py
@@ -1,34 +1,32 @@
 import random
 
-optionsForSlotMachine = { 1: "A",
-                         4: "B",
-                         3: "C",
-                         2: "D",
-                          5: "the",
-                          6: 7,
-                          7: "Neopet"}
+optionsForSlotMachine = {1: "Trash",
+                         4: "Bagpipes",
+                         3: "Loch Ness Monster",
+                         2: "Diamond",
+                         5: "Cherry",
+                         6: "Dupre",
+                         7: "Jackpot"}
 
 key = 0
 value = 0
-for x in range(5):
-    value = random.random()
-if value <= .2:
-    print(optionsForSlotMachine.get(1))
-elif .2 < value <= .4:
-    print(optionsForSlotMachine.get(2))
-elif .4 < value <= .6:
-    print(optionsForSlotMachine.get(3))
-elif .6 < value <= .7:
-    print(optionsForSlotMachine.get(4))
-elif .7 <value <= .8:
-    print(optionsForSlotMachine.get(5))
-elif.8 <value <= .97:
-    print(optionsForSlotMachine.get(6))
-elif .97 < value <= 1:
-    print(optionsForSlotMachine.get(7))
+rolled_values = []
+for x in range(3):
+    for x in range(5):
+        value = random.random()
+    if value <= .2:
+        rolled_values.append(optionsForSlotMachine.get(1))
+    elif .2 < value <= .4:
+        rolled_values.append(optionsForSlotMachine.get(2))
+    elif .4 < value <= .6:
+        rolled_values.append(optionsForSlotMachine.get(3))
+    elif .6 < value <= .7:
+        rolled_values.append(optionsForSlotMachine.get(4))
+    elif .7 < value <= .8:
+        rolled_values.append(optionsForSlotMachine.get(5))
+    elif .8 < value <= .95:
+        rolled_values.append(optionsForSlotMachine.get(6))
+    elif .95 < value <= 1:
+        rolled_values.append(optionsForSlotMachine.get(7))
 # When stop button is pressed
-#
-
-
-
-
+print(rolled_values)
