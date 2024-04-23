d1ac2146d282a8ff3f3722f7fe5693c8db00dfaf
FroggSaron <sjackso2@macalester.edu>
2024-04-23 16:34:47 -0500

    Connected buttons to calculations

--- guiEditSam.py
+++ guiEditSam.py
@@ -8,70 +8,102 @@ import PIL.Image as Image
 import PIL.ImageTk as ImageTk
 
 import os
-#------Helper Function
+
+
+# ------Helper Function-------
 def ask_bet(credit):
     while True:
         bet = simpledialog.askinteger("Your current credit is" + str(credit), "what would you like to bet")
         if int(bet) <= credit:
             return int(bet)
-            break
         else:
             messagebox.showerror("Try Again",
                                  "You don't have enough credit")
 
 
+def spin_calculations(legend):
+    rolled_value = 0
+    for i in range(5):
+        value = random.random()
+        if value <= .2:
+            rolled_value = legend.get(1)
+        elif .2 < value <= .4:
+            rolled_value = legend.get(2)
+        elif .4 < value <= .6:
+            rolled_value = legend.get(3)
+        elif .6 < value <= .7:
+            rolled_value = legend.get(4)
+        elif .7 < value <= .8:
+            rolled_value = legend.get(5)
+        elif .8 < value <= .95:
+            rolled_value = legend.get(6)
+        elif .95 < value <= 1:
+            rolled_value = legend.get(7)
+        return rolled_value
+
+
+def payout(bet, rolled_value1, rolled_value2, rolled_value3, legend_value):
+    if rolled_value1 == rolled_value2 == rolled_value3:
+        cash_multipler = legend_value.get(rolled_value1)
+        money = int(bet * cash_multipler)
+        return money
+    elif rolled_value1 == rolled_value2:
+        cash_multipler = legend_value.get(rolled_value2)
+        money = int(bet * cash_multipler/2)
+        return money
+    elif rolled_value2 == rolled_value3:
+        cash_multipler = legend_value.get(rolled_value2)
+        money = int(bet * cash_multipler/2)
+        return money
+    elif rolled_value3 == rolled_value1:
+        cash_multipler = legend_value.get(rolled_value2)
+        money = int(bet* cash_multipler/2)
+        return money
+    else:
+        return bet * 0
+
+
+def stop(bet, legend, legend_values, credit):
+    image1 = spin_calculations(legend)
+    image2 = spin_calculations(legend)
+    image3 = spin_calculations(legend)
+    print(image1,image2,image3)
+    # stopspin
+    # set each image to the corrusponding image
+    winnings = payout(bet, image1, image2, image3, legend_values)
+    credit = (credit - bet + winnings)
+    return winnings, credit
+
+
 # ----- GUI class and methods -----
 class BasicGui:
     def __init__(self):
-        # ----- Callbacks for Calculations Stuff -----
-
-        def spin_calculations(self, legend):
-            rolled_value = 0
-            for i in range(5):
-                value = random.random()
-                if value <= .2:
-                    rolled_value = legend.get(1)
-                elif .2 < value <= .4:
-                    rolled_value = legend.get(2)
-                elif .4 < value <= .6:
-                    rolled_value = legend.get(3)
-                elif .6 < value <= .7:
-                    rolled_value = legend.get(4)
-                elif .7 < value <= .8:
-                    rolled_value = legend.get(5)
-                elif .8 < value <= .95:
-                    rolled_value = legend.get(6)
-                elif .95 < value <= 1:
-                    rolled_value = legend.get(7)
-                return rolled_value
-
-        def payout(self, bet, rolled_value1, rolled_value2, rolled_value3, legend_value):
-            if rolled_value1 == rolled_value2 == rolled_value3:
-                cash_multipler = legend_value.get(rolled_value1)
-                return bet * cash_multipler
-            else:
-                return bet * 0
-
-        def stop(self, bet, legend, legend_values, credit):
-            image1 = self.spin_calculations(legend)
-            image2 = self.spin_calculations(legend)
-            image3 = self.spin_calculations(legend)
-            # stopspin
-            # set each image to the corrusponding image
-            winnings = self.payout(bet, image1, image2, image3, legend_values)
-            credit = (credit - bet + winnings)
-            return winnings, credit
-
-        def start_spin():
-           print(ask_bet(casino_credit))
 
         self.mainWin = tk.Tk()
+        # ----- Callbacks for Calculations Stuff -----
 
+        self.optionsForSlotMachine = {1: "Trash",
+                                      4: "Bagpipes",
+                                      3: "Loch Ness Monster",
+                                      2: "Apple",
+                                      5: "Diamond",
+                                      6: "Dupre",
+                                      7: "Jackpot"}
+        self.SlotMachine_value = {"Trash": .5,
+                                  "Bagpipes": 1.5,
+                                  "Loch Ness Monster": 3,
+                                  "Apple": 4,
+                                  "Diamond": 5,
+                                  "Dupre": 2,
+                                  "Jackpot": 100}
 
-
-
-
-        nameList = os.listdir("Images")  # Credit to this page for this method and the prof for mentioning it: https://www.geeksforgeeks.org/python-list-files-in-a-directory/
+        self.casino_credit = 100
+        self.winnings = 0
+        self.user_Bet = 0
+        self.spin_button = "not pressed"
+        nameList = os.listdir(
+            "Images")  # Credit to this page for this method and the prof for mentioning it:
+        # https://www.geeksforgeeks.org/python-list-files-in-a-directory/
 
         self.imgList = []
 
@@ -99,30 +131,27 @@ class BasicGui:
                           wraplength=400, justify="center")
         legend.grid(row=1, column=0)
 
-
         controlbar = tk.Frame(self.mainWin, bg="lightblue", padx=10, pady=10)
         controlbar.grid(row=0, column=1)
 
-        payoutlabel = tk.Label(controlbar)
-        payoutlabel.grid(row=0, column=1)
-        payoutlabel["text"] = "Payout:", winnings
+        self.payoutlabel = tk.Label(controlbar)
+        self.payoutlabel.grid(row=0, column=1)
+        self.payoutlabel["text"] = "Payout:", self.winnings
 
-        whattosay = "Credit:" , casino_credit
-        moneystatus = tk.Label(controlbar)
-        moneystatus.grid(row=1, column=1)
-        moneystatus["text"] = whattosay
+        whattosay = "Credit:", self.casino_credit
+        self.moneystatus = tk.Label(controlbar)
+        self.moneystatus.grid(row=1, column=1)
+        self.moneystatus["text"] = whattosay
 
         SPIN = tk.Button(controlbar)
-        SPIN['command'] = start_spin
+        SPIN['command'] = self.start_spin
         SPIN["text"] = "SPIN"
         SPIN.grid(row=2, column=1)
 
-
-
-
-        stopspin = tk.Button(controlbar)
-        stopspin["text"] = "STOP THAT WHEEL"
-        stopspin.grid(row=3, column=1)
+        stopButton = tk.Button(controlbar)
+        stopButton["text"] = "STOP THAT WHEEL"
+        stopButton['command'] = self.stop_spin
+        stopButton.grid(row=3, column=1)
 
         saying = ['life is like gambling. i just need a little more money to keep going.',
                   'Sonic says "I love Gambling"', 'gamblers dont quit. they lose.',
@@ -136,15 +165,32 @@ class BasicGui:
         hashtagdeep["bg"] = "red"
         hashtagdeep.grid(row=4, column=1)
 
-
     # def rotateImages(self):
 
     def run(self):
         self.mainWin.mainloop()
 
+    def start_spin(self):
+        self.user_Bet = ask_bet(self.casino_credit)
+        self.spin_button = "pressed"
+
+    def stop_spin(self):
+        if self.spin_button == "pressed":
+            winning_array = stop(self.user_Bet, self.optionsForSlotMachine, self.SlotMachine_value, self.casino_credit)
+            self.winnings = winning_array[0]
+            self.casino_credit = winning_array[1]
+            self.spin_button = "not pressed"
+            self.payoutlabel["text"] = "Payout:", self.winnings
+            self.moneystatus["text"] = "Credit:", self.casino_credit
+            if self.casino_credit == 0:
+                messagebox.showinfo("You Lost!!!!", "You have no credit left, Try again to make money")
+                self.casino_credit = 100
+                self.moneystatus["text"] = "Credit:", self.casino_credit
+        else:
+            messagebox.showerror("Error", "nothing to stop")
+
+
 
 # ----- Main program -----
-casino_credit = 100
-winnings = 0
 myGui = BasicGui()
 myGui.run()
